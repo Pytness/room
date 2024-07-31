378194ec00ed053a5846494015d9cf7bbabcb012
Fernando Vega <pytness@gmail.com>
2024-07-31 13:43:11 +0200

    feat(config): extract Config struct to separate module

---
+++ src/config.rs
@@ -0,0 +1,26 @@
+use std::collections::BTreeMap;
+
+#[derive(Default)]
+pub struct Config {
+    pub ignore_case: bool,
+    pub full_screen: bool,
+}
+
+impl Config {
+    pub fn from_configuration(configuration: BTreeMap<String, String>) -> Self {
+        let ignore_case = match configuration.get("ignore_case" as &str) {
+            Some(value) => value.trim().parse().unwrap(),
+            None => true,
+        };
+
+        let full_screen = match configuration.get("fullscreen" as &str) {
+            Some(value) => value.trim().parse().unwrap(),
+            None => false,
+        };
+
+        Self {
+            ignore_case,
+            full_screen,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+mod config;
+
 use owo_colors::OwoColorize;
 use std::collections::{BTreeMap, HashMap};
 use zellij_tile::prelude::*;
+
+use self::config::Config;
 
 #[derive(Default)]
 enum Mode {
@@ -21,31 +25,6 @@ impl std::fmt::Display for Mode {
                 Mode::RenameTab => "Rename Tab",
             },
         )
-    }
-}
-
-#[derive(Default)]
-struct Config {
-    ignore_case: bool,
-    full_screen: bool,
-}
-
-impl Config {
-    fn from_configuration(configuration: BTreeMap<String, String>) -> Self {
-        let ignore_case = match configuration.get("ignore_case" as &str) {
-            Some(value) => value.trim().parse().unwrap(),
-            None => true,
-        };
-
-        let full_screen = match configuration.get("fullscreen" as &str) {
-            Some(value) => value.trim().parse().unwrap(),
-            None => false,
-        };
-
-        Self {
-            ignore_case,
-            full_screen,
-        }
     }
 }
 
